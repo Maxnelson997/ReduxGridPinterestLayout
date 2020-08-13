ca75710539ada53fe4a91a698a3b4119da13f6c1
Max <maxnelson997@gmail.com>
2020-08-13 14:15:40 -0600

    added support for scss files with sass-loader, style-loaoder, and css-loader webpack plugins

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -12,11 +12,22 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ]
     },
     resolve: {
-        extensions: ['*', '.js', '.jsx']
+        extensions: ['*', '.js', '.jsx', '.scss']
     },
     output: {
       path: path.resolve(__dirname, '../', 'dist'),
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import Layout from './components/layout';
 
+import './style/main.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 
 ReactDOM.render(
     <Layout>
