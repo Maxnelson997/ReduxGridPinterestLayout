9e65c02dbeca1f9ff87016674d3f7df3a6fcff5a
Max <maxnelson997@gmail.com>
2020-08-25 17:45:48 -0600

    added redux thunk  for action creator function returns -- added dev tools extension support

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import ReactDOM from 'react-dom';
 import PinterestGrid from './components/pinterestGrid';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 
+const createStoreWithDevTools = applyMiddleware(reduxThunk)(compose((window.devToolsExtension ? window.devToolsExtension():f=>f)(createStore)))
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStoreWithDevTools(reducers)}>
         <Layout>
             <PinterestGrid title='Foto Feed'/>
             <PinterestGrid title='Favorites'/>
